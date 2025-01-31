fb98cbe38630711715623e57bd121a35acf9e765
Mor-ald <mor.ald@yandex.ru>
2025-01-31 19:12:33 +0300

    feat(types): add Tasks types

---
+++ src/types/Tasks.ts
@@ -0,0 +1,39 @@
+/**
+ * Status of tasks
+ */
+export type TaskStatus = 'to-do' | 'in-progress' | 'test' | 'closed';
+
+/**
+ * Types of tasks
+ */
+export type TaskType = 'task' | 'refactor' | 'feature' | 'error' | 'improvement';
+
+/**
+ * Priorities of tasks
+ */
+export type TaskPriority = 'low' | 'medium' | 'hard';
+
+/**
+ * Task's data
+ */
+export type Task = {
+  id: number;
+  title: string;
+  description: string;
+  status: TaskStatus;
+  type: TaskType;
+  priority: TaskPriority;
+  created: string;
+  deadline: string | null;
+  tags: [
+    {
+      name: string;
+      color: string;
+    },
+  ];
+};
+
+/**
+ * Tasks api data
+ */
+export type Tasks = Task[];
