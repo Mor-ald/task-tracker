2a9ad8c84abd8919c933f07270436255971becdb
Mor-ald <mor.ald@yandex.ru>
2025-02-01 17:54:14 +0300

    feat(components): add TaskPriority, getColor

--- src/components/ui/task/Task.tsx
+++ src/components/ui/task/Task.tsx
@@ -3,34 +3,54 @@ import ITask from './ITask';
 
 import Badge from '../badge/Badge';
 
+import { TaskPriority } from '@/types/Tasks';
+
 /**
  * Task component
  */
 export default function Task({ taskData }: ITask) {
+  const getColor = (priority: TaskPriority) => {
+    switch (priority) {
+      case 'low':
+        return '#67cb65';
+      case 'medium':
+        return '#ff9533';
+      case 'hard':
+        return '#e74444';
+      default:
+        return '#67cb65';
+    }
+  };
   return (
     <div className={styles['task']}>
       <div className={styles['task-header']}>
         <span className={styles['task-title']}>{taskData.title}</span>
-
-        <div className={styles['task-priority']}>{taskData.description}</div>
-
-        <div className={styles['task-description']}>{taskData.description}</div>
-
-        <div className={styles['task-created']}>
-          <span>Создана:</span>
-          <span>{taskData.created}</span>
-        </div>
-
-        {taskData.deadline && (
-          <div className={styles['task-deadline']}>
-            <span>Дедлайн:</span>
-            <span>{taskData.deadline}</span>
-          </div>
-        )}
-
-        {taskData.tags.length &&
-          taskData.tags.map((tag, i) => <Badge key={i} text={tag.name} color={tag.color} />)}
+        <Badge text={taskData.priority} color={getColor(taskData.priority)}></Badge>
       </div>
+      <div className={styles['task-description']}>{taskData.description}</div>
+
+      <div className={styles['task-created']}>
+        <span>Создана:</span>
+        <span>{taskData.created}</span>
+      </div>
+
+      {taskData.deadline && (
+        <div className={styles['task-deadline']}>
+          <span>Дедлайн:</span>
+          <span>{taskData.deadline}</span>
+        </div>
+      )}
+
+      {taskData.tags.length && (
+        <>
+          <div className={styles['task-text']}>#Тэги</div>
+          <div className={styles['task-tags']}>
+            {taskData.tags.map((tag, i) => (
+              <Badge key={i} text={tag.name} color={tag.color} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
