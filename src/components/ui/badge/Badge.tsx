797b2be3ba584b2342af0db768b14480530904c6
Mor-ald <mor.ald@yandex.ru>
2025-02-01 14:15:06 +0300

    refactor(components): change props, remove colors const, add style arg

--- src/components/ui/badge/Badge.tsx
+++ src/components/ui/badge/Badge.tsx
@@ -4,14 +4,10 @@ import IBadge from './IBadge';
 /**
  * Badge component
  */
-export default function Badge({ text, type }: IBadge) {
-  const colors = {
-    error: 'badge-error',
-    info: 'badge-info',
-    secondary: 'badge-secondary',
-    warning: 'badge-warning',
-    success: 'badge-success',
-  };
-
-  return <span className={`${styles['badge']} ${styles[colors[type]]}`}>{text}</span>;
+export default function Badge({ text, color }: IBadge) {
+  return (
+    <span className={`${styles['badge']}`} style={{ background: `${color}` }}>
+      {text}
+    </span>
+  );
 }
