5dd5ae406d233cc1cf2e4966c00ff651f2dc78ea
Mor-ald <mor.ald@yandex.ru>
2025-02-01 11:52:54 +0300

    refactor(components): add comments

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,6 +2,9 @@ import styles from './Layout.module.scss';
 
 import { ReactNode } from 'react';
 
+/**
+ * Layout component
+ */
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className={styles['layout']} data-testId={'layout'}>
--- src/components/ui/container/Container.tsx
+++ src/components/ui/container/Container.tsx
@@ -2,6 +2,9 @@ import styles from './Container.module.scss';
 
 import { ReactNode } from 'react';
 
+/**
+ * Container component
+ */
 export default function Container({ children }: { children: ReactNode }) {
   return (
     <div className={styles['container']} data-testId={'container'}>
