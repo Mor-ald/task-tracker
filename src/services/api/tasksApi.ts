1691f364030e5fe35c117a3d75c6031355d2ff8f
Mor-ald <mor.ald@yandex.ru>
2025-02-01 15:01:21 +0300

    feat(services): create tasks api

---
+++ src/services/api/tasksApi.ts
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+import { Task, Tasks } from '@/types/Tasks';
+
+/**
+ * Tasks API
+ */
+export const tasksApi = createApi({
+  reducerPath: 'tasksApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
+  endpoints: (builder) => ({
+    getTasks: builder.query<Tasks, string>({
+      query: () => `tasks`,
+    }),
+    addTask: builder.mutation<Tasks, Task>({
+      query: (task) => ({
+        url: `tasks`,
+        method: 'POST',
+        body: task,
+      }),
+    }),
+    updateTask: builder.mutation<Tasks, Task>({
+      query: ({ id, ...patch }) => ({
+        url: `tasks/${id}`,
+        method: 'PATCH',
+        body: patch,
+      }),
+    }),
+  }),
+});
+
+export const { useGetTasksQuery, useAddTaskMutation, useUpdateTaskMutation } = tasksApi;
