44c7fe3cf877aa1e457c2fdab11a611315b0a2c1
Mor-ald <mor.ald@yandex.ru>
2025-02-01 17:15:22 +0300

    feat: add Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import App from './App.tsx';
+import { store } from './store/rootStore.ts';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import './index.scss';
 import '@assets/styles/variables.scss';
@@ -9,6 +11,8 @@ import '@assets/styles/global.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
