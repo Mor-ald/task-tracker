697b7c9388adc0f4fa29556c4110e757c1240897
Mor-ald <mor.ald@yandex.ru>
2025-02-03 18:29:07 +0300

    feat(services): add tagTypes, providesTags and invalidatesTags

--- src/services/api/tasksApi.ts
+++ src/services/api/tasksApi.ts
@@ -7,27 +7,44 @@ import { Task, Tasks } from '@/types/Tasks';
  */
 export const tasksApi = createApi({
   reducerPath: 'tasksApi',
+  tagTypes: ['Tasks'],
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
   endpoints: (builder) => ({
     getTasks: builder.query<Tasks, void>({
       query: () => `tasks`,
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: 'Tasks' as const, id })),
+              { type: 'Tasks', id: 'LIST' },
+            ]
+          : [{ type: 'Tasks', id: 'LIST' }],
     }),
     getTaskByStatus: builder.query<Tasks, string>({
       query: (status) => `tasks?status=${status}`,
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: 'Tasks' as const, id })),
+              { type: 'Tasks', id: 'LIST' },
+            ]
+          : [{ type: 'Tasks', id: 'LIST' }],
     }),
-    addTask: builder.mutation<Tasks, Task>({
+    addTask: builder.mutation<Tasks, Omit<Task, 'id'>>({
       query: (task) => ({
         url: `tasks`,
         method: 'POST',
         body: task,
       }),
+      invalidatesTags: [{ type: 'Tasks', id: 'LIST' }],
     }),
-    updateTask: builder.mutation<Tasks, Task>({
+    updateTask: builder.mutation<Tasks, Pick<Task, 'id' | 'status'>>({
       query: ({ id, ...patch }) => ({
         url: `tasks/${id}`,
         method: 'PATCH',
         body: patch,
       }),
+      invalidatesTags: [{ type: 'Tasks', id: 'LIST' }],
     }),
   }),
 });
