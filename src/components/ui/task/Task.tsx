b8e47d8292a06c8ec5607a1085a5943ab301de57
Mor-ald <mor.ald@yandex.ru>
2025-02-03 19:30:45 +0300

    feat(components): add update task's data in form by double click

--- src/components/ui/task/Task.tsx
+++ src/components/ui/task/Task.tsx
@@ -2,13 +2,19 @@ import styles from './Task.module.scss';
 import ITask from './ITask';
 
 import Badge from '../badge/Badge';
+import { onSetNewCurrentTitle, toggleSideBar } from '../sidebar/SideBarSlice';
+import { onSetTask } from '../task-form/TaskFormSlice';
 
 import { TaskPriority } from '@/types/Tasks';
+import { useAppDispatch, useAppSelector } from '@/hooks/hooks';
 
 /**
  * Task component
  */
 export default function Task({ taskData }: ITask) {
+  const dispatch = useAppDispatch();
+  const open = useAppSelector((store) => store.sidebar.open);
+
   const getColor = (priority: TaskPriority) => {
     switch (priority) {
       case 'low':
@@ -21,8 +27,31 @@ export default function Task({ taskData }: ITask) {
         return '#67cb65';
     }
   };
+
+  const onDoubleClick = () => {
+    if (!open) {
+      dispatch(onSetNewCurrentTitle({ title: 'Редактирование задачи' }));
+      dispatch(
+        onSetTask({
+          id: taskData.id,
+          title: taskData.title,
+          description: taskData.description,
+          type: taskData.type,
+          status: taskData.status,
+          priority: taskData.priority,
+          created: taskData.created,
+          deadline: taskData.deadline
+            ? taskData.deadline.split('-').reverse().join('-')
+            : taskData.deadline,
+          tags: taskData.tags,
+        }),
+      );
+      dispatch(toggleSideBar());
+    }
+  };
+
   return (
-    <div className={styles['task']}>
+    <div className={styles['task']} onDoubleClick={onDoubleClick}>
       <div className={styles['task-header']}>
         <span className={styles['task-title']}>{taskData.title}</span>
         <Badge text={taskData.priority} color={getColor(taskData.priority)}></Badge>
