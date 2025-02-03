eebd01f39ff70a37e3c886a33010485cf783c6bd
Mor-ald <mor.ald@yandex.ru>
2025-02-03 18:30:56 +0300

    feat(components): add toggleSideBar method to button

--- src/components/ui/button/plus-button/PlusButton.tsx
+++ src/components/ui/button/plus-button/PlusButton.tsx
@@ -1,13 +1,17 @@
 import styles from './PlusButton.module.scss';
 
 import PlusCircleIcon from '../../icons/PlusCircleIcon';
+import { toggleSideBar } from '../../sidebar/SideBarSlice';
+
+import { useAppDispatch } from '@/hooks/hooks';
 
 /**
  * Plus button component
  */
 export default function PlusButton() {
+  const dispatch = useAppDispatch();
   return (
-    <div className={styles['button']}>
+    <div className={styles['button']} onClick={() => dispatch(toggleSideBar())}>
       <PlusCircleIcon />
     </div>
   );
