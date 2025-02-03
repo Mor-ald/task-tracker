a77ecbd5f5e573dcec6e6c28ad44d63147f2b474
Mor-ald <mor.ald@yandex.ru>
2025-02-03 18:39:24 +0300

    feat(components): add currentTitle, onSetNewCurrentTitle

--- src/components/ui/sidebar/SideBarSlice.ts
+++ src/components/ui/sidebar/SideBarSlice.ts
@@ -3,15 +3,19 @@ import { createSlice } from '@reduxjs/toolkit';
 export const sideBarSlice = createSlice({
   name: 'sideBar',
   initialState: {
+    currentTitle: '',
     open: false,
   },
   reducers: {
     toggleSideBar: (state) => {
       state.open = !state.open;
     },
+    onSetNewCurrentTitle: (state, action) => {
+      state.currentTitle = action.payload.title;
+    },
   },
 });
 
-export const { toggleSideBar } = sideBarSlice.actions;
+export const { toggleSideBar, onSetNewCurrentTitle } = sideBarSlice.actions;
 
 export default sideBarSlice.reducer;
