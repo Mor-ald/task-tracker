465e0151c52aa162121c8349c2968c131a65ce13
Mor-ald <mor.ald@yandex.ru>
2025-02-04 17:07:12 +0300

    fix(components): remove type prop, add color prop, fix tests

--- src/components/ui/badge/Badge.test.tsx
+++ src/components/ui/badge/Badge.test.tsx
@@ -6,11 +6,12 @@ import { render, screen } from '@testing-library/react';
 
 describe('Badge', () => {
   it('Renders correctly', () => {
-    render(<Badge text="badge" type="error" />);
+    render(<Badge text="badge" color="#000" />);
 
     const badgeByText = screen.getByText('badge');
 
     expect(badgeByText).toBeInTheDocument();
-    expect(badgeByText).toHaveClass('badge badge-error');
+    expect(badgeByText).toHaveClass('badge');
+    expect(badgeByText.style.background).toBe('rgb(0, 0, 0)');
   });
 });
