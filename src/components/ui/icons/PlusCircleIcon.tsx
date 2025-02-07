8e1f688ca50b9bd4d7090cd898fa7b9f722b2c9b
Mor-ald <mor.ald@yandex.ru>
2025-02-07 12:50:14 +0300

    refactor(components): fix attributes

--- src/components/ui/icons/PlusCircleIcon.tsx
+++ src/components/ui/icons/PlusCircleIcon.tsx
@@ -8,23 +8,23 @@ export default function PlusCircleIcon() {
         d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
         fill="#67CB65"
         stroke="#67CB65"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M12 8V16"
         stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M8 12H16"
         stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
